c3ba73082483ade5e7e1f36c0b60eba707be429c
Helio Barbosa <barbosahifb1997@gmail.com>
2026-07-31 09:24:10 -0100

    Aulas java

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,195 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Servico {
-    String titulo;
-    String descricao;
-    Double preco;
-    Boolean estrativo;
+    private String titulo;
+    private String descricao;
+    private Double preco;
+    private Boolean estaAtivo;
 
-    public Servico(String novoTitulo, String novoDiscricao, Double novoPreco, Boolean novoEstrativo) {
+    public Servico(String novoTitulo, String novoDiscricao, Double novoPreco, Boolean novoEstaAtivo) {
         this.titulo = novoTitulo;
         this.descricao = novoDiscricao;
         this.preco = novoPreco;
-        this.estrativo = novoEstrativo;
+        this.estaAtivo = novoEstaAtivo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getTitulo() {
+        return this.titulo;
+    }
+    public void setDescricao( String descricao) {
+        this.descricao = descricao;
+    }
+    public String getDescricao() {
+        return this.descricao;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public double getPreco() {
+        return this.preco;
+    }
+    public void setEstaAtivo (boolean estaAtivo) {
+        this.estaAtivo = estaAtivo;
+    }
+    public boolean getEstaAtivo () {
+        return this.estaAtivo;
+    }
+
+    public void aplicarDesconto(double percentagem) {
+        double valorDesconto = (this.preco * percentagem) / 100;
+        this.preco = this.preco - valorDesconto;
+
+        System.out.println("------------- Desconto ---------------");
+        System.out.println("Desconto Aplicado com sucesso");
+        System.out.println("valor final: " + this.preco);
+    }
+    public void verificarDisponibilidade() {
+        if (this.estaAtivo) {
+            System.out.println("Servico " + this.titulo + " esta disponivel");
+        } else {
+            System.out.println("Servico " + this.titulo + " nao esta disponivel");
+        }
     }
 }
 
 class Pessoa {
-    String nome;
-    String morada;
-    int idade;
+    private String nome;
+    private String morada;
+    private int idade;
+    private Double saldo;
 
-    public Pessoa(String novoNome, String novaMorada, int novaIdade) {
+    List<Servico> servicosComprado;
+
+    public Pessoa(String novoNome, String novaMorada, int novaIdade, Double novoSaldo) {
         this.nome = novoNome;
         this.morada = novaMorada;
         this.idade = novaIdade;
+        this.saldo = novoSaldo;
+        this.servicosComprado = new ArrayList<>();
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+    public String getMorada() {
+        return this.morada;
+    }
+
+    public void setIdade(String morada) {
+        this.morada = morada;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public  void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+
+    public void comprarServico(Double saldo, Servico servicoEscolhido) throws Exception {
+        if (this.saldo < servicoEscolhido.getPreco() || !servicoEscolhido.getEstaAtivo() == true) {
+
+            System.out.println("Erro: Saldo insuficiente ou serviço inativo.");
+            throw  new Exception("Erro insuficiente ou Servico inativo");
+        }
+
+        this.saldo -= servicoEscolhido.getPreco();
+        this.servicosComprado.add(servicoEscolhido);
+        System.out.println("Compra efetuada com sucesso");
+    }
+
+    public void mostrarHistorico() {
+        System.out.println("----------Lista de servicos comprados--------------");
+        for(Servico s: this.servicosComprado){
+            System.out.println("Servico: " + s.getTitulo());
+        }
     }
 };
+class ServicoDigital extends Servico {
+    String linkDownload;
+
+    public ServicoDigital(String titulo, String descricao, double preco, boolean estaAtivo) {
+        super (titulo, descricao, preco, estaAtivo);
+        this.linkDownload = linkDownload;
+    }
+}
+class Vendedor extends Pessoa {
+    Double taxaComissao;
+    public Vendedor(String nome, String morada, int telefone, double saldo , double taxaComissao) {
+        super(nome, morada, telefone, saldo);
+        this.taxaComissao = taxaComissao;
+    }
+    public double getTaxaComissao() {
+        return taxaComissao;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         Servico Servico = new Servico("Desenvolvidor Full Stack", "Criar e executar aplicações web e moveis",120.00, true);
 
         System.out.println("------------------ Servico --------------------------");
-        System.out.println("titulo : " + Servico.titulo);
-        System.out.println("Discricao: " + Servico.descricao);
-        System.out.println("preco : " + Servico.preco);
-        System.out.println("estado: " + Servico.estrativo);
+        System.out.println("titulo : " + Servico.getTitulo());
+        System.out.println("Discricao: " + Servico.getDescricao());
+        System.out.println("preco : " + Servico.getPreco());
+        System.out.println("estado: " + Servico.getEstaAtivo());
 
-        Servico Servico1 = new Servico("Manutencao CPU", "tecnico", 12.11,true);
+        Servico servico1 = new Servico("Manutencao CPU", "tecnico", 12.11,true);
 
         System.out.println("------------------ Servico1 -------------------------");
-        System.out.println("titulo : " + Servico1.titulo);
-        System.out.println("Discricao: " + Servico1.descricao);
-        System.out.println("preco : " + Servico1.preco);
-        System.out.println("estado: " + Servico1.estrativo);
+        System.out.println("titulo : " + servico1.getTitulo());
+        System.out.println("Discricao: " + servico1.getDescricao());
+        System.out.println("preco : " + servico1.getPreco());
+        System.out.println("estado: " + servico1.getEstaAtivo());
 
-        Pessoa pessoa1 = new Pessoa("Helio", "Ponta d´ água", 29);
+        servico1.aplicarDesconto(10);
+
+        servico1.verificarDisponibilidade();
+
+        Pessoa pessoa1 = new Pessoa("Helio", "Ponta d´ água", 29, 20000.0);
+
+        Vendedor vendedor = new Vendedor("Augusto","achadinha", 9877656, 200, 1222);
+
+        Servico servicoEscolhido = new Servico("Pintor", "realizar pinturas", 1500.0, true);
+
+        try {
+            pessoa1.comprarServico(pessoa1.getSaldo(), servicoEscolhido);
+        } catch (Exception e) {
+            System.out.println("Ocorreu o seguinte erro: " + e.getMessage());
+        }
+
 
         System.out.println("----------------- Pessoa1 --------------------");
-        System.out.println("nome da pessoa1: " + pessoa1.nome);
-        System.out.println("morada da pessoa1: " + pessoa1.morada );
-        System.out.println("idade da pessoa1: " + pessoa1.idade);
+        System.out.println("nome da pessoa1: " + pessoa1.getNome());
+        System.out.println("morada da pessoa1: " + pessoa1.getMorada() );
+        System.out.println("idade da pessoa1: " + pessoa1.getIdade());
         System.out.println("--------------------------------------------");
 
-        System.out.println("-------- Simulação de Compra ----------");
-        System.out.println("O " + pessoa1.nome + " comprou o serviço " + Servico1.titulo + " por " + Servico1.preco + " CVE.");
+        System.out.println("----------- Vendedor ---------------");
+        System.out.println("Nome: " + vendedor.getNome());
+        System.out.println("Morada: " + vendedor.getMorada());
+        System.out.println("Idade: " + vendedor.getIdade());
+        System.out.println("Saldo: " + vendedor.getSaldo());
+        System.out.println("Taxa de comissão = " + vendedor.getTaxaComissao());
+
+        System.out.println("--------  Compra ----------");
+        System.out.println("O " + pessoa1.getNome() + " comprou o serviço " + servico1.getTitulo() + " por " + servico1.getPreco() + " CVE.");
 
         System.out.println("--------------------------------------------");
         System.out.println("Hello Word");
